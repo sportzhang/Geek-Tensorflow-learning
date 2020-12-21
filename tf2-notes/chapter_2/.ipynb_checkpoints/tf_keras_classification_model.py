d10faa8d130c4c186daef9c305446baca9ae2450
zhangkai <zkai@8531.cn>
2020-12-21 19:46:01 +0800

    add tf2 chapter2 jupyter files

---
+++ tf2-notes/chapter_2/.ipynb_checkpoints/tf_keras_classification_model.py
@@ -0,0 +1,25 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+
+from tensorflow import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(x_train_all, y_train_all), (x_test, y_test) = fashion_mnist.load_data()
+x_valid, x_train = x_train_all[:5000], x_train_all[5000:]
+y_valid, y_train = y_train_all[:5000], y_train_all[5000:]
+
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+print(x_valid.shape, y_valid.shape)
---
+++ tf2-notes/chapter_2/data_util.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import os
+import gzip
+import numpy as np
+
+
+def load_data():
+    """Loads the Fashion-MNIST dataset.
+
+  This is a dataset of 60,000 28x28 grayscale images of 10 fashion categories,
+  along with a test set of 10,000 images. This dataset can be used as
+  a drop-in replacement for MNIST. The class labels are:
+
+  | Label | Description |
+  |:-----:|-------------|
+  |   0   | T-shirt/top |
+  |   1   | Trouser     |
+  |   2   | Pullover    |
+  |   3   | Dress       |
+  |   4   | Coat        |
+  |   5   | Sandal      |
+  |   6   | Shirt       |
+  |   7   | Sneaker     |
+  |   8   | Bag         |
+  |   9   | Ankle boot  |
+
+  Returns:
+      Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+
+      **x_train, x_test**: uint8 arrays of grayscale image data with shape
+        (num_samples, 28, 28).
+
+      **y_train, y_test**: uint8 arrays of labels (integers in range 0-9)
+        with shape (num_samples,).
+
+  License:
+      The copyright for Fashion-MNIST is held by Zalando SE.
+      Fashion-MNIST is licensed under the [MIT license](
+      https://github.com/zalandoresearch/fashion-mnist/blob/master/LICENSE).
+
+  """
+    dirname = os.path.join('datasets', 'fashion-mnist')
+    # 数据下载到本地
+    base = 'data/'
+    # base = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/'
+    files = [
+        'train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
+        't10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz'
+    ]
+
+    paths = [base + f_name for f_name in files]
+    # for fname in files:
+    #   paths.append(get_file(fname, origin=base + fname, cache_subdir=dirname))
+
+    with gzip.open(paths[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+    with gzip.open(paths[1], 'rb') as imgpath:
+        x_train = np.frombuffer(
+            imgpath.read(), np.uint8, offset=16).reshape(len(y_train), 28, 28)
+
+    with gzip.open(paths[2], 'rb') as lbpath:
+        y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+    with gzip.open(paths[3], 'rb') as imgpath:
+        x_test = np.frombuffer(
+            imgpath.read(), np.uint8, offset=16).reshape(len(y_test), 28, 28)
+
+    return (x_train, y_train), (x_test, y_test)
